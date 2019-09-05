ad44f57e8e5eb0516613b0004e16038a0672fc68
Gerardo Ramirez <gerardoramirez@users.noreply.github.com>
2019-09-04 21:23:23 -0700

    Adding technical challenge submission
    Hi Charlie,
    
    Attached is my solution.  It was a great experience as I was able to play with the BrowserStack Automate product.  I'm impressed with the features and how it helps developers run their tests.  My code is based on an example found in the links provided:
    
    -https://www.browserstack.com/automate/java
    -https://www.browserstack.com/automate/java
    -https://www.browserstack.com/automate/capabilities?tag=selenium-4#
    
     mvn test -P parallel

--- src/test/java/com/testSuite/SingleTest.java
+++ src/test/java/com/testSuite/SingleTest.java
@@ -1,16 +1,25 @@
 package com.testSuite;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class SingleTest extends TestNGTest {
 
     @Test
     public void test() throws Exception {
 
+        System.out.println("process test() ...");
+
         driver.get("https://www.browserstack.com/");
+
         String headerText = driver.findElement(By.id("primary-menu")).getText();
         Assert.assertTrue(headerText.contains("Sign in"));
         Assert.assertFalse(headerText.contains("Account"));
@@ -18,6 +27,37 @@ public class SingleTest extends TestNGTest {
         WebElement RightDevicesLink  = driver.findElement(By.id("signupModalButton"));
         RightDevicesLink.click();
 
+        Thread.sleep(5000);
         Assert.assertEquals(driver.getTitle(),"Get Started For Free & Access 2000+ Mobile Devices & Browsers | BrowserStack");
+
+        // Added this to hide the cookie policy banner that was showing at the bottom of the page in the videos to verify the test.
+        //accept-cookie-notification
+        WebElement acceptCookieNotification  = driver.findElement(By.id("accept-cookie-notification"));
+        acceptCookieNotification.click();
+
+        Thread.sleep(5000);
+
+        driver.findElement(By.id("user_full_name")).sendKeys("Gerardo Ramirez");
+        driver.findElement(By.id("user_email_login")).sendKeys("familiaramirez1999@gmail.com");
+        driver.findElement(By.id("user_password")).sendKeys("technicalchallenge1qaz");
+
+        /*
+         * I did have problems with the checkbox and upon some research I found this thread to aide in my implmentation of checking the box and submitting the form
+         * https://stackoverflow.com/questions/50061394/org-openqa-selenium-elementnotinteractableexception-element-is-not-reachable-b?noredirect=1&lq=1
+         *
+         */
+        WebElement agreeToTerms = driver.findElement(By.id("tnc_checkbox"));
+        ((JavascriptExecutor)driver).executeScript("arguments[0].click();",agreeToTerms);
+
+        //
+        // Confirming that the agree to terms box was indeed checked
+        System.out.println("Is agreeToTerms selected: " + agreeToTerms.isSelected());
+
+        WebElement userSubmit = driver.findElement(By.id("user_submit"));
+        ((JavascriptExecutor)driver).executeScript("arguments[0].click();",userSubmit);
+
+
+
     }
+
 }
--- src/test/java/com/testSuite/TestNGTest.java
+++ src/test/java/com/testSuite/TestNGTest.java
@@ -1,19 +1,74 @@
 package com.testSuite;
 
+import org.json.simple.JSONObject;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.json.simple.parser.JSONParser;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.BeforeMethod;
+
+import java.io.FileReader;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 public class TestNGTest {
+
     public WebDriver driver;
 
-    @BeforeMethod(alwaysRun=true)
-    public void setUp() throws Exception {
-        driver = new ChromeDriver();
-    }
 
+    @BeforeMethod(alwaysRun=true)
+    @org.testng.annotations.Parameters(value={"config", "environment"})
+    public void setUp(String config_file, String environment) throws Exception {
+        JSONParser parser = new JSONParser();
+        JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
+        JSONObject envs = (JSONObject) config.get("environments");
+
+       // System.out.println("obtained the conf file and envs");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        Map<String, String> envCapabilities = (Map<String, String>) envs.get(environment);
+        Iterator it = envCapabilities.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
+        }
+
+        Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
+        it = commonCapabilities.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            if(capabilities.getCapability(pair.getKey().toString()) == null){
+                capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
+            }
+        }
+
+        String username = System.getenv("BROWSERSTACK_USERNAME");
+        if(username == null) {
+            username = (String) config.get("user");
+        }
+
+        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        if(accessKey == null) {
+            accessKey = (String) config.get("key");
+        }
+
+        String app = System.getenv("BROWSERSTACK_APP_ID");
+        if(app != null && !app.isEmpty()) {
+            capabilities.setCapability("app", app);
+        }
+
+        driver = new RemoteWebDriver(new URL("http://"+username+":"+accessKey+"@"+config.get("server")+"/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+    }
     @AfterMethod(alwaysRun=true)
     public void tearDown() throws Exception {
         driver.quit();
